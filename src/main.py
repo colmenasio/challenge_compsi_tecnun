a9273abef61699136e39c1dc6b5918ffd6fda8c1
colmenasio <a.colmenero.fernandez@gmail.com>
2025-03-14 12:36:38 +0100

    [Feat] Added more more basic processing

--- src/main.py
+++ src/main.py
@@ -8,9 +8,14 @@ if __name__ == '__main__':
     # Remove missing values
     df_sanitized = utils.remove_invalid_ages(utils.remove_age_na(df_raw))
 
-    # Partition df according to direction of traffic
-    outbound_df, inbound_df, inner_df = utils.traffic_based_partition(df_raw)
+    # Further develop the dataframe
+    utils.expand(df_sanitized, in_place=True)
+    utils.summary(df_sanitized)
 
-    print(f"Outbound total: {outbound_df["personas"].sum()}")
-    print(f"Inbound total: {inbound_df["personas"].sum()}")
-    print(f"Inner total: {inner_df["personas"].sum()}")
+    # Partition df according to direction of traffic
+    outbound_df, inbound_df, inner_df = utils.traffic_based_partition(df_sanitized)
+    del df_raw, df_sanitized
+
+    print(f"Outbound total: {outbound_df["avrg_displacements"].sum()}")
+    print(f"Inbound total: {inbound_df["avrg_displacements"].sum()}")
+    print(f"Inner total: {inner_df["avrg_displacements"].sum()}")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import filter
 import consts
 
 def summary(df: pd.DataFrame) -> None:
+    pd.set_option("display.max_columns", None)
     print(df.head())
     print(df.shape)
     print(df.dtypes)
@@ -44,3 +45,31 @@ def traffic_based_partition(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFram
     inner_traffic_df = df[df['destino'].isin(consts.donostia_ids) & df['origen'].isin(consts.donostia_ids) ]
     assert df.shape[0] == outbound_df.shape[0] + inbound_df.shape[0] + inner_traffic_df.shape[0]
     return outbound_df, inbound_df, inner_traffic_df
+
+def expand(i_df: pd.DataFrame,
+           in_place : bool = True,
+           parse_date: bool = True,
+           average_age: bool = True,
+           average_recurrence: bool = True,
+           get_number_of_displacements: bool = True,
+           ) -> pd.DataFrame:
+    """For average_age age must not be a NAN value"""
+    if not in_place:
+        df = i_df.copy()
+    else:
+        df = i_df
+
+    if parse_date:
+        df["years"] = df["mes"].apply(lambda x : 0 if not isinstance(x, int) or len(str(x)) != 6 else int(str(x)[0:4]))
+        df["month"] = df["mes"].apply(lambda x : 0 if not isinstance(x, int) or len(str(x)) != 6 else int(str(x)[4:6]))
+
+    if average_age:
+        df["avrg_age"] = df["edad"].apply(lambda x: (max(x.left, 18) + max(x.right, 18))/2 if isinstance(x, pd.Interval) else -1)
+
+    if average_recurrence:
+        df["avrg_recurrence"] = df["recurrencia"].apply(lambda x: (x.right + x.left)/2 if isinstance(x, pd.Interval) else 1)
+
+    if get_number_of_displacements and average_recurrence:
+        df["avrg_displacements"] = df["personas"] * df["avrg_recurrence"]
+
+    return  df
